83c2aa1584f4e3ea68c8458222fa4958509ed604
huewilliams <4879sinwoo@naver.com>
2019-01-17 23:59:50 +0900

    UPDATE: db connection info

--- Cannabis_Backend/models/index.js
+++ Cannabis_Backend/models/index.js
@@ -5,9 +5,9 @@ const db = {};
 const sequelize = new Sequelize(
     'cannabis', //db 이름
     'root', // 이름명
-    'espada8012!', // 비밀번호
+    '1234', // 비밀번호
     {
-        'host': '127.0.0.1', // 사용할 호스트
+        'host': '13.59.174.126', // 사용할 호스트
         'dialect': 'mysql', // 사용할 DB 종류
         'operatorsAliases':false //deprecated 된 연산자 사용
     }
