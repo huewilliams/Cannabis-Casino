fa967560a027f1b1b3c756c19bc96c3f0743a14a
huewilliams <4879sinwoo@naver.com>
2019-01-19 22:24:59 +0900

    CREATE: main vue instance

---
+++ Cannabis_Frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div id="content">
+      <Profile></Profile>
+      <Contents></Contents>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import Profile from './components/Profile';
+  import Contents from './components/Contents';
+
+  export default {
+    name: 'app',
+    components:{
+      Header,
+      Profile,
+      Contents
+    }
+  }
+</script>
+
+<style>
+  #content {
+    display: flex;
+  }
+  h1 {
+    color:crimson;
+  }
+</style>
