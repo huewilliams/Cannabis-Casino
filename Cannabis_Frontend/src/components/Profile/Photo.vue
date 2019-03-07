325710a1472cf5fa6d497fe4e2776efc1a646c09
huewilliams <4879sinwoo@naver.com>
2019-03-07 19:56:45 +0900

    CREATE: photo component - use axios to get user profile image

---
+++ Cannabis_Frontend/src/components/Profile/Photo.vue
@@ -0,0 +1,32 @@
+<template>
+  <img v-bind:src="url" v-bind:id="edit()">
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        url: 'hue'
+      }
+    },
+    methods: {
+      edit: //엑시오스 GET 요청
+      function() {
+        axios.get('http://localhost:5000/user/')
+          .then((response) => {
+            this.url = response.data;
+          })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 200px;
+    height: 200px;
+    margin: 0;
+    padding: 0;
+    border-radius: 5px;
+  }
+</style>
