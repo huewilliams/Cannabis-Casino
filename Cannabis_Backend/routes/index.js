fd14d169e90451efb472d213965e97ce2b0bb35d
huewilliams <4879sinwoo@naver.com>
2019-03-08 14:23:16 +0900

    UPDATE: GET/new API redirect signup page

--- Cannabis_Backend/routes/index.js
+++ Cannabis_Backend/routes/index.js
@@ -8,4 +8,8 @@ router.get('/', jwtVerify, (req, res) => {
     res.render('index.html');
 });
 
+router.get('/new', (req, res)=> {
+   res.redirect('signup/signup.html');
+});
+
 module.exports = router;
