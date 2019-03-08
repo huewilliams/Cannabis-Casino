59176d91ddc6112b77d400376864e858a2bbb296
huewilliams <4879sinwoo@naver.com>
2019-03-08 17:05:40 +0900

    CREATE: GET/ user/nick API

--- Cannabis_Backend/routes/user.js
+++ Cannabis_Backend/routes/user.js
@@ -2,15 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 const { verify } = require('./middlewares');
+const User = require('../models').User;
 
-router.get('/img/:nickname', (req, res)=> {
-    //let token = req.cookies.token;
-    //let userInfo = verify(token);
-    //res.send(`http://localhost:5000/${userInfo.nickname}.jpg`);
-    res.send(`huewilliams`);
+router.get('/img/:nickname', async (req, res, next)=> {
+    try {
+        let user = await User.findOne({
+            where: {nickname: req.params.nickname}
+        });
+        res.send(`http://localhost:5000/${user.profile}`);
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.get('/', (req,res)=>{
+router.get('/nick', (req,res)=>{
     let token = req.cookies.token;
     let userInfo = verify(token);
     res.send(userInfo.nickname);
