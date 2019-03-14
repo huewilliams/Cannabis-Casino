afcb1abab9874a20a3cb491c5a29e6d3ae5d9f9d
huewilliams <4879sinwoo@naver.com>
2019-03-14 15:17:12 +0900

    UPD: image upload API

--- Cannabis_Backend/routes/user.js
+++ Cannabis_Backend/routes/user.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 const { verify } = require('./middlewares');
@@ -31,6 +34,30 @@ router.get('/yam', (req, res)=>{
     let token = req.cookies.token;
     let userInfo = verify(token);
     res.send(String(userInfo.yam));
+});
+
+fs.readdir('uploads', (error)=>{
+    if (error) {
+        console.error('uploads 폴더가 없어 uploads 폴더를 서버의 디스크에 생성합니다');
+        fs.mkdirSync('uploads');
+    }
+});
+
+let upload = multer({
+    storage: multer.diskStorage({
+        // 파일 저장 경로 설정
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
+        }
+        ,
+        // 파일 사이즈 제한 설정
+        limits: {fileSize: 5 * 1024 * 1024}
+    })
+});
+
+router.post('/dev/img', upload.single('img'), async (req, res)=> {
+//    console.log(req.file);
+    res.json({ url : `http://${process.env.HOST}:5000/${req.file.filename}`});
 });
 
 module.exports = router;
