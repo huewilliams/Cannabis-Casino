d048c81bd3bde141f4b69c57ea2a43ff17ceeea1
huewilliams <4879sinwoo@naver.com>
2019-03-14 17:03:36 +0900

    UPD: dotenv module

--- Cannabis_Backend/app.js
+++ Cannabis_Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+require('dotenv').config();
 const favicon = require('express-favicon');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
