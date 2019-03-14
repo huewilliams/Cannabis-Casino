3dd86b938c523f5f03c1d1905db9d670dd805ea3
huewilliams <4879sinwoo@naver.com>
2019-03-14 09:33:16 +0900

    UPD: change DB server host

--- Cannabis_Backend/models/index.js
+++ Cannabis_Backend/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     'root', // 이름명
     '1234', // 비밀번호
     {
-        'host': '13.59.174.126', // 사용할 호스트
+        'host': '18.220.117.207', // 사용할 호스트
         'dialect': 'mysql', // 사용할 DB 종류
         'operatorsAliases':false //deprecated 된 연산자 사용
     }
