473a56f82cc84c1c08c6432fd9968b155705925d
huewilliams <4879sinwoo@naver.com>
2019-03-17 22:28:59 +0900

    CRE: profile name component(axios)

---
+++ Cannabis_Frontend/src/components/Profile/Name.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>{{ nick }}</h3>
+</template>
+
+<script>
+    export default {
+        name: "Name",
+      data() {
+          return {
+            nick : ''
+          }
+      },
+      beforeCreate :function () {
+        axios.get('http://18.220.117.207:5000/user/nick')
+          .then((res)=> {
+              console.log(res.data);
+              this.nick = res.data;
+            }
+          )
+      }
+    }
+</script>
+
+<style scoped>
+  h3 {
+    color: white;
+    text-align: center;
+  }
+</style>
