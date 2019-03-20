7d6fa4a09abbb33037bdf55ed58294bff1de6eff
huewilliams <4879sinwoo@naver.com>
2019-03-20 18:35:41 +0900

    UPD: POST /poker - add people and bet

--- Cannabis_Backend/routes/poker.js
+++ Cannabis_Backend/routes/poker.js
@@ -8,6 +8,8 @@ router.post('/', jwtVerify, async (req, res)=>{
     const room = await Room.create({
         title: req.body.title,
         owner: req.body.owner,
+        people: 1,
+        bet: req.body.bet,
     });
     res.send('game room created');
 });
