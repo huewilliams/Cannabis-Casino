17a33a5f33bd9f857953f30fcc4dd07f2600b7b0
huewilliams <4879sinwoo@naver.com>
2019-03-22 08:24:47 +0900

    CRE: casino main page

---
+++ Cannabis_Backend/views/build/precache-manifest.8fb2bc11e204fbfb19b8f54fcaf045c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d10fb818740369a2cac4623948a65d6",
+    "url": "/static/media/Poker.2d10fb81.jpg"
+  },
+  {
+    "revision": "a6c2f5b91fe5e646818c0d359f7ce5fb",
+    "url": "/static/media/CANNABIS.a6c2f5b9.png"
+  },
+  {
+    "revision": "ab4a280461ff25ca5f4890c997356393",
+    "url": "/static/media/Banner.ab4a2804.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9c72872b99ded104ff70",
+    "url": "/static/js/main.6aaf1e1f.chunk.js"
+  },
+  {
+    "revision": "994ac0a8b245405beab1",
+    "url": "/static/js/2.32e40f14.chunk.js"
+  },
+  {
+    "revision": "9c72872b99ded104ff70",
+    "url": "/static/css/main.ea23fb4f.chunk.css"
+  },
+  {
+    "revision": "e295705d5ef8cdc4a5a5c49545e43202",
+    "url": "/index.html"
+  }
+];
