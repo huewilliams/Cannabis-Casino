329c5cc2e60d73b7d30e775889074e92a757492c
huewilliams <4879sinwoo@naver.com>
2019-03-22 08:29:26 +0900

    UPD: set main page path

--- Cannabis_Backend/routes/index.js
+++ Cannabis_Backend/routes/index.js
@@ -4,12 +4,19 @@ const { jwtVerify } = require('./middlewares');
 
 let router = express.Router();
 
-router.get('/', jwtVerify, (req, res) => {
-    res.render('index.html');
+router.get('/', (req, res) => {
+    res.render('./build/index.html');
 });
 
 router.get('/new', (req, res)=> {
    res.redirect('signup/signup.html');
 });
 
+router.get('/req', (req, res)=> {
+    res.json({time: global.time});
+});
+
+router.get('/join', (req, res)=>{
+    res.render('indianPoker', {title:'소켓 테스토'});
+});
 module.exports = router;
