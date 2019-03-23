fa3bbc67f8afc64304f420c1a6a3b53aa69984c4
huewilliams <4879sinwoo@naver.com>
2019-03-23 15:38:55 +0900

    UPD: 방 입장 API 예외처리 및 방 리스트 렌더링 API제작

--- Cannabis_Backend/routes/poker.js
+++ Cannabis_Backend/routes/poker.js
@@ -14,23 +14,37 @@ router.post('/', async (req, res)=>{
     res.send('game room created');
 });
 
-router.get('/', async (req, res)=>{
+router.get('/list', async (req, res)=>{
     const room = await Room.findAll({});
     res.json(room);
 });
 
+router.get('/', (req, res)=>{
+   res.render('room.html');
+});
+
 router.get('/:title', async (req, res)=>{
-   const room = await Room.findOne({
-       where: {title: req.params.title}
-   });
-   if(room) {
-       res.send('duplicate');
-   } else {
-       res.send('OK');
-       const io = req.app.get('io');
-       res.render('indianPoker', {title: req.params.title});
-       io.of('/game').emit('new', req.params.title);
-   }
+    const room = await Room.findOne({
+        where: {title: req.params.title}
+    });
+    if(room)
+        res.render('indianPoker', {title: req.params.title});
+    else
+        res.send('존재하지 않는 방입니다');
+});
+
+router.get('/check/:title', async (req, res)=>{
+    const room = await Room.findOne({
+        where: {title: req.params.title}
+    });
+    if (room)
+        res.send('duplicate');
+    else
+        res.send('OK');
+});
+
+router.post('/enter', (req, res)=>{
+    res.send(req.body.title);
 });
 
 module.exports = router;
