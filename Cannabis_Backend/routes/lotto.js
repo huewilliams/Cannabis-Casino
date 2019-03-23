5f6c7e1216300f74f0329e8ebf3998cae50d8446
huewilliams <4879sinwoo@naver.com>
2019-03-23 20:48:29 +0900

    MOD: lotto 검증 로직 수정

--- Cannabis_Backend/routes/lotto.js
+++ Cannabis_Backend/routes/lotto.js
@@ -4,19 +4,24 @@ const random = require('random');
 let router = express.Router();
 
 const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
+const client = redis.createClient('18078', 'redis-18078.c10.us-east-1-2.ec2.cloud.redislabs.com');
 
-client.auth(process.env.REDIS_PASSWORD);
-client.on('error', (err)=>{
+client.auth('Br34hJvfhfuBY32qWS9Z69mO02cKcRMS');
+client.on('error', (err) => {
     console.log(err);
 });
 
-router.post('/set', async (req, res)=>{
-    if(req.body.secret != process.env.REDIS_SECRET)
+router.post('/set', async (req, res) => {
+    if (req.body.secret != 'cannabis_secret')
         res.send('접근권한이 없습니다');
+
+    await client.flushdb(function (err, succeeded) {
+        console.log(succeeded);
+    });
+
     let list = Array(45)
         .fill()
-        .map((element, index)=>{
+        .map((element, index) => {
             return index + 1;
         });
 
@@ -26,16 +31,14 @@ router.post('/set', async (req, res)=>{
         list[rand] = list[i];
         list [i] = temp;
     }
-    await client.flushdb(function (err, succeeded) {
-        console.log(succeeded);
-    });
+
     list.join('');
-    let select = list.splice(0,7);
-    console.log('새로운 로또 번호 : ',select);
+    let select = list.splice(0, 7);
+    console.log('새로운 로또 번호 : ', select);
     select.forEach(x => {
         client.lpush('lotto', x);
     });
-    let check = client.lrange('lotto',0,client.llen('lotto')-1);
+    let check = client.lrange('lotto', 0, client.llen('lotto') - 1);
     res.json(check);
 });
 
@@ -57,7 +60,7 @@ router.post('/check', async (req, res) => {
 
     let user = req.body.lotto.split('-');
 
-    for (let i = 0; i < user.length - 1; i++) {
+    for (let i = 0; i < user.length; i++) {
         for (let j = 0; j < check.length - 1; j++) {
             if (user[i] == check[j])
                 count++;
